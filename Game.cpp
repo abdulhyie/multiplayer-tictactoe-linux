e8a4420991616d0735222b504e4af4fb347d1529
Abdul Hai <abdulhyyie@gmail.com>
2020-06-09 20:34:15 +0500

    Update Game.cpp
    Much improved version of previous one. Runs on linux as well.
    
    ONE MAJOR FLAW:
    While generating random values for x and y for CPU turn, it may take forever to get a pair of suitable values if board is clustered. Will fix that later.

--- Game.cpp
+++ Game.cpp
@@ -1,313 +1,184 @@
-//This code spaghetti and awful. Doesn't run on linux.
-
-#include<iostream>
-#include<iomanip>
-#include<cstdlib>
-#include<windows.h>
-#include<ctime>
+#include <iostream>
+#include <string>
+//#include <ncurses.h>
+#include <ctime>
+#include <unistd.h>
 
 using namespace std;
 
-void menu();
-void display();
-void input();
-void gameplay();
+class TicTacToe {
+private:
+	int mode;
+	string p1Name, p2Name;
+	bool gameOver = false;
+	bool turns = false;
+	char board[3][3];
+public:
+	TicTacToe() {
+		mode = 3;		//assign any value other than 1 or 2
+		//initialize the board
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				board[i][j] = '-';
+			}
+		}
+	}
+	bool checkWinningCond() {
+		//This checks for all the columns
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 2; j++) {
+				if (board[j][i] == '-' || board[j][i] != board[j + 1][i]) {
+					break;
+				}
+				if (j == 1) {
+					return true;
+				}
+			}
+		}
+		//This checks for all the rows
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 2; j++) {
+				if (board[i][j] == '-' || board[i][j] != board[i][j + 1]) {
+					break;
+				}
+				if (j == 1) {
+					return true;
+				}
+			}
+		}
+		// this checks for right diagonal
+		for (int i = 0; i < 2; i++) {
+			if (board[i][i] == '-' || board[i][i] != board[i + 1][i + 1])
+				break;
+			if (i == 1) {
+				return true;
+			}
+		}
+		//this checks for left diagonal
+		for (int i = 0; i < 2; i++) {
+			if (board[2 - i][i] == '-' || board[2 - i][i] != board[2 - i - 1][i + 1])
+				break;
+			if (i == 1) {
+				return true;
+			}
+		}
+		return false;
+	}
+	void menu()
+	{
+		while (mode != 1 && mode != 2) {
+			cout << "WELCOME! Please select the mode you want to play." << endl << endl << "1- 1v1\t\t\t2- 1vComputer" << endl << endl;
+			cout << "Enter: ";
+			cin >> mode;
+			if (mode != 1 && mode != 2) {
+				cout << "Invalid input. Please Enter again." << endl;
+			}
+			//If user selects 1v1, ask both player names
+			if (mode == 1) {
+				cout << "Player 1 Name: ";
+				cin >> p1Name;
+				cout << "Player 2 Name: ";
+				cin >> p2Name;
+			}
+			//if user selects 1vComp, ask only player's name
+			else {
+				cout << "Player Name: ";
+				cin >> p1Name;
+				p2Name = "CPU";
+			}
+		}
+	//	clear();
+	}
+	void displayBoard()
+	{
+		cout << "\tMode: " << p1Name << " vs. " << p2Name << endl;
+		cout << "\t" << p1Name << ": X\t\t" << p2Name << ": 0" << endl << endl << endl;
+		cout << " 	 Column 0   Column 1   Column 2 " << endl
+			<< " 	________________________________" << endl
+			<< " 	|         |          |         |" << endl
+			<< " Row 0	|    " << board[0][0] << "    |     " << board[0][1] << "    |    " << board[0][2] << "    |" << endl
+			<< " 	|_________|__________|_________|" << endl
+			<< " 	|         |          |         |" << endl
+			<< " Row 1	|    " << board[1][0] << "    |     " << board[1][1] << "    |    " << board[1][2] << "    |" << endl
+			<< " 	|_________|__________|_________|" << endl
+			<< " 	|         |          |         |" << endl
+			<< " Row 2	|    " << board[2][0] << "    |     " << board[2][1] << "    |    " << board[2][2] << "    |" << endl
+			<< " 	|_________|__________|_________|" << endl << endl << endl;
+	}
 
-int mode;
-bool gameOver = false;
-bool turns = false;
-int rows, cols;
-char mark[3][3];
-
-int main()
-{
-	menu();
-	while (!gameOver){
-		display();
-		input();
-		gameplay();
-		system("cls");
-		if (turns == true){
+	void changeTurn() {
+		if (turns == true) {
 			turns = false;
 		}
 		else
 			turns = true;
 	}
-	if (turns == false){
-		display();
-		cout << endl << endl << "Player (2) won the game.";
-	}
-	else{
-		display();
-		cout << endl << endl << "Player (1) won the game.";
-	}
-	system("pause>0");
-}
 
-void menu()
+	void getXY() {
+		int x, y;
+		if (mode == 2 && !turns) {
+			x = y = 3;
+			do {
+				srand(time(0));
+				x = rand() % 3;
+				y = rand() % 3;
+			} while (board[x][y] == 'X' || board[x][y] == 'O');
+		}
+		else {
+			do {
+				x = y = 3;		//give any value exept 0, 1, 2
+				while (x < 0 || x > 2) {
+					cout << "Enter row: ";
+					cin >> x;
+					if (x < 0 || x > 2)	cout << "Invalid!" << endl;
+				}
+				while (y < 0 || y > 2) {
+					cout << "Enter column: ";
+					cin >> y;
+					if (y < 0 || y > 2)	cout << "Invalid!" << endl;
+				}
+				if (board[x][y] == 'X' || board[x][y] == 'O') {
+					cout << "Place alreday occupied!" << endl;
+				}
+			} while (board[x][y] == 'X' || board[x][y] == 'O');
+		}
+		placeToken(x, y);
+	}
+
+	void placeToken(int x, int y) {
+		if (turns) {
+			board[x][y] = 'O';
+		}
+		else board[x][y] = 'X';
+	}
+	void run() {
+		menu();
+		while (!gameOver) {
+			displayBoard();
+			getXY();
+			changeTurn();
+			if (checkWinningCond()) {
+			//	clear();
+				displayBoard();
+				if (!turns) {
+					cout << endl << endl << p2Name << " has WON the game." << endl;
+				}
+				else {
+					cout << endl << endl << p1Name << " has WON the game." << endl;
+				}
+				cout << "Press any key to continue." << endl;
+			//	getch();
+				return;
+			}
+		//	clear();
+		}
+		cout << "Press any key to continue." << endl;
+	//	getch();
+	}
+};
+
+int main()
 {
-	cout << setw(35) << ".:: TicTacToe ::." << endl << setw(40) << ".:: Made by Abdul Hai ::." << endl << endl << endl
-		<< "WELCOME! Please select the mode you want to play." << endl << endl << "1- 1v1\t\t\t2- 1vComputer" << endl << endl ;
-mode_check:
-	cout << "Enter: ";
-	cin >> mode;
-	if (mode != 1 && mode != 2){
-		cout << "Invalid input. Please Enter again." << endl;
-		goto mode_check;
-	}
-	system("cls");
-}
-void display()
-{
-	cout << setw(35) << ".:: TicTacToe ::." << endl << setw(40) << ".:: Made by Abdul Hai ::." << endl;
-	if (mode == 1){
-		cout << setw(31) << "Mode: 1v1" << endl << endl << endl;
-	}
-	else
-		cout << setw(35) << "Mode: 1vComputer" << endl << endl << endl;
-	cout << " 	 Column 0   Column 1   Column 2 " << endl
-		<<	" 	________________________________" << endl
-		<< " 	|         |          |         |" << endl
-		<< " Row 0	|    " << mark[0][0] << "    |     " << mark[0][1] << "    |    " << mark[0][2] << "    |" << endl
-		<< " 	|_________|__________|_________|" << endl
-		<< " 	|         |          |         |" << endl
-		<< " Row 1	|    " << mark[1][0] << "    |     " << mark[1][1] << "    |    " << mark[1][2] << "    |" << endl
-		<< " 	|_________|__________|_________|" << endl
-		<< " 	|         |          |         |" << endl
-		<< " Row 2	|    " << mark[2][0] << "    |     " << mark[2][1] << "    |    " << mark[2][2] << "    |" << endl
-		<< " 	|_________|__________|_________|" << endl;
-}
-void input()
-{
-	if (mode == 1){
-		if (turns == false){
-		row_check:
-			cout << endl << "-------------------------------------------" << endl << endl;
-			cout << "Player (1) turn: ";
-			cout << endl << "Enter Row: ";
-			cin >> rows;
-			if (rows > 2 || rows < 0){
-				cout << "Invalid input. Enter Again.";
-				goto row_check;
-			}
-		col_check:
-			cout << "Enter colums: ";
-			cin >> cols;
-			if (cols > 2 || cols < 0){
-				cout << "Invalid input. Enter Again.";
-				goto col_check;
-			}
-			if (mark[rows][cols] == 'X' || mark[rows][cols] == 'O'){
-				cout << endl << "Spot is already occupied. Enter again." << endl;
-				goto row_check;
-			}
-			else
-				mark[rows][cols] = 'X';
-		}
-		else{
-		row_check1:
-			cout << endl << "-------------------------------------------" << endl << endl;
-			cout << "Player (2) turn: ";
-			cout << endl << "Enter Row: ";
-			cin >> rows;
-			if (rows > 2 || rows < 0){
-				cout << "Invalid input. Enter Again.";
-				goto row_check1;
-			}
-		col_check1:
-			cout << "Enter colums: ";
-			cin >> cols;
-			if (cols > 2 || cols < 0){
-				cout << "Invalid input. Enter Again.";
-				goto col_check1;
-			}
-			if (mark[rows][cols] == 'X' || mark[rows][cols] == 'O'){
-				cout << endl << "Spot is already occupied. Enter again." << endl;
-				goto row_check1;
-			}
-			else
-				mark[rows][cols] = 'O';
-		}
-	}
-	else{
-		if (turns == false){
-		row_check3:
-			cout << endl << "-------------------------------------------" << endl << endl;
-			cout << "Your turn: ";
-			cout << endl << "Enter Row: ";
-			cin >> rows;
-			if (rows > 2 || rows < 0){
-				cout << "Invalid input. Enter Again.";
-				goto row_check3;
-			}
-		col_check3:
-			cout << "Enter colums: ";
-			cin >> cols;
-			if (cols > 2 || cols < 0){
-				cout << "Invalid input. Enter Again.";
-				goto col_check3;
-			}
-			if (mark[rows][cols] == 'X' || mark[rows][cols] == 'O'){
-				cout << endl << "Spot is already occupied. Enter again." << endl;
-				goto row_check3;
-			}
-			else
-				mark[rows][cols] = 'X';
-		}
-		else{
-			cout << endl << "-------------------------------------------" << endl << endl;
-			cout << "Computer turn. ";
-		row_check2:
-			rows = rand() % 3;
-			cols = rand() % 3;
-			cout << cols;
-			if (mark[rows][cols] == 'X' || mark[rows][cols] == 'O'){
-				goto row_check2;
-			}
-			else{
-				cout << endl << "Row: ";
-				Sleep(1000);
-				cout << rows << endl;;
-				cout << "Colums: ";
-				Sleep(1000);
-				cout << cols;
-				mark[rows][cols] = 'O';
-			}
-		}
-	}
-}
-void gameplay()
-{
-	if (turns == false){
-		for (int i = 0; i<3; i++){
-			if (mark[0][i] != 'X'){
-				break;
-			}
-			if (i == 2){
-				gameOver = true;
-			}
-		}
-		for (int i = 0; i<3; i++){
-			if (mark[1][i] != 'X'){
-				break;
-			}
-			if (i == 2){
-				gameOver = true;
-			}
-		}
-		for (int i = 0; i<3; i++){
-			if (mark[2][i] != 'X'){
-				break;
-			}
-			if (i == 2){
-				gameOver = true;
-			}
-		}
-		for (int i = 0; i<3; i++){
-			if (mark[i][0] != 'X'){
-				break;
-			}
-			if (i == 2){
-				gameOver = true;
-			}
-		}
-		for (int i = 0; i<3; i++){
-			if (mark[i][1] != 'X'){
-				break;
-			}
-			if (i == 2){
-				gameOver = true;
-			}
-		}
-		for (int i = 0; i<3; i++){
-			if (mark[i][2] != 'X'){
-				break;
-			}
-			if (i == 2){
-				gameOver = true;
-			}
-		}
-		for (int i = 0; i<3; i++){
-			if (mark[i][i] != 'X'){
-				break;
-			}
-			if (i == 2){
-				gameOver = true;
-			}
-		}
-		for (int i = 0, j = 2; i<3; i++, j--){
-			if (mark[i][j] != 'X'){
-				break;
-			}
-			if (i == 2){
-				gameOver = true;
-			}
-		}
-	}
-	else{
-		for (int i = 0; i<3; i++){
-			if (mark[0][i] != 'O'){
-				break;
-			}
-			if (i == 2){
-				gameOver = true;
-			}
-		}
-		for (int i = 0; i<3; i++){
-			if (mark[1][i] != 'O'){
-				break;
-			}
-			if (i == 2){
-				gameOver = true;
-			}
-		}
-		for (int i = 0; i<3; i++){
-			if (mark[2][i] != 'O'){
-				break;
-			}
-			if (i == 2){
-				gameOver = true;
-			}
-		}
-		for (int i = 0; i<3; i++){
-			if (mark[i][0] != 'O'){
-				break;
-			}
-			if (i == 2){
-				gameOver = true;
-			}
-		}
-		for (int i = 0; i<3; i++){
-			if (mark[i][1] != 'O'){
-				break;
-			}
-			if (i == 2){
-				gameOver = true;
-			}
-		}
-		for (int i = 0; i<3; i++){
-			if (mark[i][2] != 'O'){
-				break;
-			}
-			if (i == 2){
-				gameOver = true;
-			}
-		}
-		for (int i = 0; i<3; i++){
-			if (mark[i][i] != 'O'){
-				break;
-			}
-			if (i == 2){
-				gameOver = true;
-			}
-		}
-		for (int i = 0, j = 2; i<3; i++, j--){
-			if (mark[i][j] != 'O'){
-				break;
-			}
-			if (i == 2){
-				gameOver = true;
-			}
-		}
-	}
+	TicTacToe game;
+	game.run();
 }
