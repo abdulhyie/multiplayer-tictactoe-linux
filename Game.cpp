573d5353c63c5aea0f1990a411b94779bea99486
Abdul Hai <abdulhyyie@gmail.com>
2020-06-10 13:53:26 +0500

    TicTacToe with threads functionality added. Runs on linux only.
    Runs on linux.
    
    2 threads have been added.
    
    Thread 1: (t_time)
    Calculate time that player takes while making a move
    
    Thread 2: (t_high_score)
    Reads and writes high score in a file names 'high-score.txt'
    
    NOTE:
    Follow the given pattern for text file,
    player_1_score (space) player_2_score

--- Game.cpp
+++ Game.cpp
@@ -1,27 +1,74 @@
 #include <iostream>
 #include <string>
-//#include <ncurses.h>
 #include <ctime>
+#include <fstream>
 #include <unistd.h>
+#include <pthread.h>
+#include <chrono>
+#include <fcntl.h> 
+#include <sys/stat.h> 
+#include <sys/types.h>
+#include <stdlib.h>
 
 using namespace std;
+using namespace std::chrono;
+
+int p1Score = 0, p2Score = 0, p1HScore, p2HScore;
+
+//threads data
+pthread_mutex_t time_lock;
+pthread_cond_t xy_read;
+bool done;
+pthread_t t_time, t_high_score;
+
+//threads functions
+void* getTime(void *args) {
+	auto start = high_resolution_clock::now();
+	while (!done) {
+		//wait for the signal
+	}
+	auto stop = high_resolution_clock::now();
+	auto duration = duration_cast<seconds>(stop - start);
+	cout << endl << "Time taken to select: " << duration.count() << " seconds." << endl << endl << endl;
+}
+void* writeHighScore(void *args) {
+	fstream in;
+	in.open("high-score.txt", ios::in);
+	int score1, score2;
+	in >> score1 >> score2;
+	if (p1Score > score1){
+		score1 = p1Score;
+	}
+	if (p2Score > score2){
+		score2 = p2Score;
+	}
+	in.close();
+	fstream out;
+	out.open("high-score.txt", ios::out);
+	out << score1 << ' ' << score2;
+	out.close();
+}
 
 class TicTacToe {
 private:
-	int mode;
+	int mode, occupiedPlaces;
 	string p1Name, p2Name;
-	bool gameOver = false;
-	bool turns = false;
+	bool gameOver;
+	bool turns;
 	char board[3][3];
 public:
 	TicTacToe() {
+		gameOver = turns = false;
+		occupiedPlaces = 9;
 		mode = 3;		//assign any value other than 1 or 2
-		//initialize the board
+		//initialize the board with '-'
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
 				board[i][j] = '-';
 			}
 		}
+		time_lock = PTHREAD_MUTEX_INITIALIZER;
+		xy_read = PTHREAD_COND_INITIALIZER;
 	}
 	bool checkWinningCond() {
 		//This checks for all the columns
@@ -91,8 +138,12 @@ public:
 	}
 	void displayBoard()
 	{
-		cout << "\tMode: " << p1Name << " vs. " << p2Name << endl;
-		cout << "\t" << p1Name << ": X\t\t" << p2Name << ": 0" << endl << endl << endl;
+		cout << endl << endl << "----------------------------------" << endl << endl;
+		cout << "Mode: " << p1Name << " vs. " << p2Name << endl << endl;
+		cout << "" << p1Name << ": X\t\t" << p2Name << ": 0" << endl << endl;
+		cout << p1Name << " Score: " << p1Score << "\t\t" << p2Name << " Score: " << p2Score << endl << endl; 
+		if (!turns)	cout << p1Name << "'s turn: O" << endl << endl;
+		else cout << p2Name << "'s turn: X" << endl << endl;
 		cout << " 	 Column 0   Column 1   Column 2 " << endl
 			<< " 	________________________________" << endl
 			<< " 	|         |          |         |" << endl
@@ -105,7 +156,6 @@ public:
 			<< " Row 2	|    " << board[2][0] << "    |     " << board[2][1] << "    |    " << board[2][2] << "    |" << endl
 			<< " 	|_________|__________|_________|" << endl << endl << endl;
 	}
-
 	void changeTurn() {
 		if (turns == true) {
 			turns = false;
@@ -113,7 +163,6 @@ public:
 		else
 			turns = true;
 	}
-
 	void getXY() {
 		int x, y;
 		if (mode == 2 && !turns) {
@@ -125,6 +174,8 @@ public:
 			} while (board[x][y] == 'X' || board[x][y] == 'O');
 		}
 		else {
+			done  = false;
+			pthread_create(&t_time, NULL, getTime, NULL);
 			do {
 				x = y = 3;		//give any value exept 0, 1, 2
 				while (x < 0 || x > 2) {
@@ -141,10 +192,13 @@ public:
 					cout << "Place alreday occupied!" << endl;
 				}
 			} while (board[x][y] == 'X' || board[x][y] == 'O');
+			done = true;	//signal the thread;
+			pthread_join(t_time, NULL);
 		}
 		placeToken(x, y);
+		x = y = 3;
+		occupiedPlaces--;
 	}
-
 	void placeToken(int x, int y) {
 		if (turns) {
 			board[x][y] = 'O';
@@ -154,31 +208,63 @@ public:
 	void run() {
 		menu();
 		while (!gameOver) {
+			system("clear");
 			displayBoard();
 			getXY();
 			changeTurn();
 			if (checkWinningCond()) {
-			//	clear();
+				system("clear");
 				displayBoard();
 				if (!turns) {
 					cout << endl << endl << p2Name << " has WON the game." << endl;
+					p2Score++;
 				}
 				else {
 					cout << endl << endl << p1Name << " has WON the game." << endl;
+					p1Score++;
 				}
-				cout << "Press any key to continue." << endl;
-			//	getch();
+				//update the high scores in high-score.txt file
+				pthread_create(&t_high_score, NULL, writeHighScore, NULL);
+				pthread_join(t_high_score, NULL);
 				return;
 			}
-		//	clear();
+			if (occupiedPlaces == 0){
+				cout << endl << endl << "It's a draw, no one won!" << endl;
+			}
 		}
-		cout << "Press any key to continue." << endl;
-	//	getch();
+	}
+	bool reset(){
+		char opt = 'x';
+		while (opt != 'y' && opt != 'Y' && opt != 'n' && opt != 'N'){
+			cout << "Do you want tp play gain? [y/n]: ";
+			cin >> opt;
+			if (opt != 'y' && opt != 'Y' && opt != 'n' && opt != 'N'){
+				cout << "Invalid selection!" << endl;
+			}
+		}
+		cout << endl << "-------------------------" << endl << endl;
+		if (opt == 'n' || opt == 'N')	return false;
+		else{
+			gameOver = turns = false;
+		occupiedPlaces = 9;
+		mode = 3;		//assign any value other than 1 or 2
+		//initialize the board with '-'
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				board[i][j] = '-';
+			}
+		}
+		time_lock = PTHREAD_MUTEX_INITIALIZER;
+		xy_read = PTHREAD_COND_INITIALIZER;
+		return true;
+		}
 	}
 };
 
 int main()
 {
 	TicTacToe game;
-	game.run();
+	do{
+		game.run();
+	}while (game.reset());
 }
